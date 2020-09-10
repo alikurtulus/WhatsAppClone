0c258329d972a93cb8f386b5b815446dbc6ad828
alikurtulush92 <kopanarya92@gmail.com>
2020-09-10 01:25:36 +0100

    added sidebarchat

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
       <div className="app_body">
-        {/* Sidebar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>
