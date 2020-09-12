a136063edb9ee3e081e9c496de6352862881b3c4
alikurtulush92 <kopanarya92@gmail.com>
2020-09-12 22:51:23 +0100

    added chat

--- whatsappclone/src/components/Chat/Chat.js
+++ whatsappclone/src/components/Chat/Chat.js
@@ -1,6 +1,8 @@
 import React,{useState, useEffect} from 'react'
 import './Chat.css'
 import {Avatar, IconButton} from '@material-ui/core'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import MicIcon from '@material-ui/icons/Mic'
 import {AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons'
 const  Chat = () => {
     const [seed,setSeed] = useState('')
@@ -28,10 +30,19 @@ const  Chat = () => {
                </div>
             </div>
             <div className="chat_body">
-
+              <p className={`chat_message ${true && 'chat_receiver'}`}>
+              <span className="chat_name">Ali K</span>
+                Hey Guys!
+                <span className="chat_timestamp">13:23</span>
+              </p>
             </div>
             <div className="chat_footer">
-
+              <InsertEmoticonIcon />
+              <form>
+                 <input type="text" /> 
+                 <button>Send a message</button>
+              </form>
+              <MicIcon />
             </div>
         </div>
     )
