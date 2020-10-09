ce02e885f87cbf9f1f4a013d262bcaf906be1d93
alikurtulush92 <kopanarya92@gmail.com>
2020-10-09 15:41:47 +0100

    added chat footer style

--- whatsappclone/src/components/Chat/Chat.js
+++ whatsappclone/src/components/Chat/Chat.js
@@ -30,17 +30,22 @@ const  Chat = () => {
                </div>
             </div>
             <div className="chat_body">
+              <p className='chat_message'>
+                <span className="chat_name">Ertan</span>
+                  Napan cuz?
+                  <span className="chat_timestamp">{new Date().toUTCString()}</span>
+              </p>
               <p className={`chat_message ${true && 'chat_receiver'}`}>
               <span className="chat_name">Ali K</span>
-                Hey Guys!
-                <span className="chat_timestamp">13:23</span>
+               I'm good cuz,you?
+                <span className="chat_timestamp">{new Date().toUTCString()}</span>
               </p>
             </div>
             <div className="chat_footer">
               <InsertEmoticonIcon />
               <form>
-                 <input type="text" /> 
-                 <button>Send a message</button>
+                 <input type="text" placeholder="Type a message" /> 
+                 <button type="submit" >Send a message</button>
               </form>
               <MicIcon />
             </div>
