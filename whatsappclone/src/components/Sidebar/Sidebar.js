4dd525586c1de7380afbf114c5fe617ad0ddef93
alikurtulush92 <kopanarya92@gmail.com>
2020-10-13 23:26:47 +0100

    added a new room option

--- whatsappclone/src/components/Sidebar/Sidebar.js
+++ whatsappclone/src/components/Sidebar/Sidebar.js
@@ -5,44 +5,81 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge'
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchOutlined from '@material-ui/icons/SearchOutlined'
+import AddIcon from '@material-ui/icons/Add';
 import SidebarChat from './SidebarChat'
 import axios from '../../axios'
 
 const Sidebar = () => {
   const [rooms,setRooms] = useState([])
+  const [searchTerm,setSearchTerm] = useState('')
+  const [searchResults,setSearchResults] = useState([])
+  const [isSearched,setIsSearched] = useState(false)
+  const [seed,setSeed] = useState('')
+ 
   useEffect(() => {
+    if(searchTerm === ''){
+      axios.get('/api/v1/rooms/sync')
+      .then(res => {
+        setRooms(res.data)
+        setSearchResults(res.data)
+        setSeed(Math.floor(Math.random() * 5000))
+      })
+    }
+    else{
+        const getResults = rooms.filter( r => r.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        console.log(getResults.length)
+        setSearchResults(getResults)
+        if(getResults.length === 0){
+          setIsSearched(true)
+        }
+        else{
+          setIsSearched(false)
+        }
+    }
+  },[searchTerm])
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+
+  }
+  const addNewChat = async (e) => {
+    e.preventDefault()
+    await axios.post('/api/v1/rooms/new',{
+      "name":searchTerm,
+      "image":`https://avatars.dicebear.com/api/human/${seed}.svg`
+    })
+    setSearchTerm('')
     axios.get('/api/v1/rooms/sync')
     .then(res => {
-      console.log(res.data)
-      setRooms(res.data)
+      setSearchResults(res.data)
     })
-  },[])
+
+  }
+
     return (
         <div className="sidebar">
             <div className="sidebar_header">
               <Avatar />
               <div className="sidebar_headerRight">
-                 <IconButton>
-                   <DonutLargeIcon />
-                 </IconButton>
-                 <IconButton>
-                   <ChatIcon />
-                 </IconButton>
-                 <IconButton> 
-                   <MoreVertIcon />
-                 </IconButton>
+                <IconButton>
+                  <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                  <ChatIcon />
+                </IconButton>
+                <IconButton> 
+                  <MoreVertIcon />
+                </IconButton>
               </div>
             </div>
             <div className="sidebar_search">
               <div className="sidebar_searchContainer">
-                <SearchOutlined />
-                <input placeholder="Search or start new chat" type="text"/>
+                {!isSearched ? ( <SearchOutlined />):(<a onClick={addNewChat} className="btn_chat_add"><AddIcon /></a>)} 
+                <input placeholder={!isSearched ? 'Search or start new chat' : 'Add a new chat'} type="text" onChange={handleChange} />
               </div>
             </div>
             <div className="sidebar_chats">
-              <SidebarChat addNewChat />
-              {rooms.map(room => (
-                <SidebarChat key={room._id} id={room._id} room={room}  />
+              {searchResults.map(room => (
+                <SidebarChat key={room._id} id={room._id} room={room} />
               ))}
               
             
--- whatsappclone/src/components/Sidebar/SidebarChat.js
+++ whatsappclone/src/components/Sidebar/SidebarChat.js
@@ -1,41 +1,27 @@
 import React, {useState, useEffect} from 'react'
 import './SidebarChat.css'
 import {Avatar} from '@material-ui/core'
-import axios from '../../axios'
+import {Link} from 'react-router-dom'
 
-const SidebarChat = ({addNewChat,room}) => {
-    const [rooms,setRooms] = useState([])
-    useEffect(() => {
-        axios.get('/api/v1/rooms/sync')
-        .then(res => {
-            setRooms(res.data)
-        })
-    },[])
-    const createChat = () =>{
-        const roomName = prompt("Please enter name for chat")
-        const isRoomNew = rooms.filter( r => r.name === roomName)
-        if(isRoomNew.length !== 0){
-           //do some clever database stuff
-
-        }
-        else{
-            alert('This room already exists')
-        }
+const SidebarChat = ({room,id}) => {
+  
+    const openChat = () =>{
+      
+        
     }
 
-    return  !addNewChat ? (
-        <div className="sidebarChat">
-            <Avatar src={room.image} />
-            <div className="sidebarChat_info">
-            <h3>{room.name}</h3>
-                <p>Last message...</p>
+    return (
+        <Link to={`/rooms/${id}`}>
+            <div  className="sidebarChat">
+                <Avatar src={room.image} />
+                <div className="sidebarChat_info">
+                    <h3>{room.name}</h3>
+                    <p>Last message...</p>
+                </div>
             </div>
-        </div>
-    ) : (
-        <div onClick={createChat} className="sidebarChat">
-            <h2>Add new chat</h2>
-        </div>
-    )
+        </Link>
+        )
+    
 }
 
 export default SidebarChat
