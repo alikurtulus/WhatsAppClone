c13c3723dbb413ff8e8dff88065887a49ed6bd61
alikurtulush92 <kopanarya92@gmail.com>
2020-10-17 23:21:19 +0100

    added all feature

--- whatsapp-backend/models/HttpError.js
+++
@@ -1,7 +0,0 @@
-class HttpError extends Error {                                                 // We extends some properties from Error class 
-    constructor(message,errorCode){
-      super(message)
-      this.code = errorCode
-    }
-  }
-  module.exports = HttpError
